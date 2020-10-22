7952f390638afb5579dbd7640fd947c4eaef037d
Jhenderson19 <69118664+Jhenderson19@users.noreply.github.com>
2020-10-21 23:49:58 -0400

    Complete functional-shared stack and queue

--- sprint-one/src/functional-shared/queue.js
+++ sprint-one/src/functional-shared/queue.js
@@ -1,8 +1,29 @@
 var Queue = function() {
-  // Hey! Rewrite in the new style. Your code will wind up looking very similar,
-  // but try not not reference your old code in writing the new style.
+  var queue = {};
+  queue.storage = {};
+  queue.length = 0;
+  return _.extend(queue, queueMethods);
 };
 
-var queueMethods = {};
+var queueMethods = {
+  enqueue(value) {
+    this.storage[this.length] = value;
+    this.length++;
+  },
+  dequeue() {
+    var value;
+    if (this.length !== 0) {
+      value = this.storage[0];
+      for (var i = 0; i < this.length; i++) {
+        this.storage[i] = this.storage[i + 1];
+      }
+      delete this.storage[this.length - 1];
+      this.length--;
+    }
+    return value;
+  },
+  size() {
+    return this.length;
+  }
 
-
+};
--- sprint-one/src/functional-shared/stack.js
+++ sprint-one/src/functional-shared/stack.js
@@ -1,8 +1,25 @@
 var Stack = function() {
-  // Hey! Rewrite in the new style. Your code will wind up looking very similar,
-  // but try not not reference your old code in writing the new style.
+  var stack = {};
+  stack.storage = {};
+  stack.length = 0;
+  return _.extend(stack, stackMethods);
 };
 
-var stackMethods = {};
-
-
+var stackMethods = {
+  push(value) {
+    this.storage[this.length] = value;
+    this.length++;
+  },
+  pop() {
+    var value;
+    if ( this.length !== 0 ) {
+      value = this.storage[this.length - 1];
+      delete this.storage[this.length - 1];
+      this.length--;
+    }
+    return value;
+  },
+  size() {
+    return this.length;
+  }
+};
