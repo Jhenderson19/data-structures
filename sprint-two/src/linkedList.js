5cd9ec75cb2ac70e77e2524a138603ff160c46d5
James Henderson <jhende1995@gmail.com>
2020-10-27 00:23:24 -0400

    Complete Linked List

--- sprint-two/src/linkedList.js
+++ sprint-two/src/linkedList.js
@@ -1,15 +1,42 @@
 var LinkedList = function() {
   var list = {};
-  list.head = null;
-  list.tail = null;
+  list.head = new Node(undefined);
+  list.tail = list.head;
+
+  list.head.next = list.tail;
 
   list.addToTail = function(value) {
+    if (list.tail.value === undefined) {
+      list.tail.value = value;
+    } else {
+      var newTail = new Node(undefined);
+      list.tail.next = newTail;
+      list.tail = newTail;
+      list.tail.value = value;
+    }
   };
 
   list.removeHead = function() {
+    var removedHead = list.head.value;
+    list.head = list.head.next;
+
+    return removedHead;
   };
 
   list.contains = function(target) {
+    var traverseAndCompare = function (currentNode) {
+      if (currentNode.value === target) {
+        return true;
+      } else {
+        if (currentNode.next) {
+          return traverseAndCompare(currentNode.next);
+        } else {
+          return false;
+        }
+      }
+    };
+
+    return traverseAndCompare(list.head);
   };
 
   return list;
