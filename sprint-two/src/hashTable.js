826cbd2086d35ad291a669d899ce452485489105
James Henderson <jhende1995@gmail.com>
2020-10-28 23:39:58 -0400

    Complete Set

--- sprint-two/src/hashTable.js
+++ sprint-two/src/hashTable.js
@@ -7,20 +7,52 @@ var HashTable = function() {
 
 HashTable.prototype.insert = function(k, v) {
   var index = getIndexBelowMaxForKey(k, this._limit);
+
+  if (this._storage[index] === undefined) {
+    this._storage[index] = [];
+  } else {
+    for (var i = 0; i < this._storage[index].length; i++) {
+      if (this._storage[index][i][0] === k) {
+        this._storage[index][i][1] = v;
+        return;
+      }
+    }
+  }
+
+  this._storage[index].push([k, v]);
 };
 
 HashTable.prototype.retrieve = function(k) {
   var index = getIndexBelowMaxForKey(k, this._limit);
+
+  var bucket = this._storage[index];
+
+  for (var i = 0; i < bucket.length; i++) {
+    var tuple = bucket[i];
+
+    if (tuple[0] === k) {
+      return tuple[1];
+    }
+  }
 };
 
 HashTable.prototype.remove = function(k) {
   var index = getIndexBelowMaxForKey(k, this._limit);
+
+  var bucket = this._storage[index];
+
+  for (var i = 0; i < bucket.length; i++) {
+    var tuple = bucket[i];
+
+    if (tuple[0] === k) {
+      bucket.splice(i, 1);
+    }
+  }
 };
 
 
 
 /*
  * Complexity: What is the time complexity of the above functions?
+ * In relation to the size of the entire hashtable, these functions are constant time.
  */
-
-
